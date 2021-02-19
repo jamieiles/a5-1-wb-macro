2b81d34d71c2ea0154386d65642ded9605a147f9
Jamie Iles <jamie@jamieiles.com>
2021-02-19 01:08:12 +0000

    macro: increase to ~140MHz

--- macro/config.tcl
+++ macro/config.tcl
@@ -13,5 +13,5 @@ set ::env(FP_SIZING) absolute
 set ::env(DESIGN_IS_CORE) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
-set ::env(CLOCK_PERIOD) "10.000"
+set ::env(CLOCK_PERIOD) "7.500"
 set ::env(CLOCK_PORT) "wb_clk_i"
